6e3b521b8290ca8c5732d81d990616d94ce5cd81
İlaç Asistan Developer <developer@ilacasistan.com>
2025-12-14 23:32:33 +0300

    .

---
+++ f06_container_kullanimi/android/app/src/main/kotlin/com/example/f06_container_kullanimi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.f06_container_kullanimi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ f06_container_kullanimi/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Container Kullanımı'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          border: Border.all(color: Colors.blue, width: 5),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Text("Merhaba"),
+      ),
+    );
+  }
+}
